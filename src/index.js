c69f9b016b94f3886f515d01512649b164855971
Sean McLeod <seanmcleod93@gmail.com>
2020-09-26 16:46:38 +1000

    Create design system, button components, default and dark themes

--- src/index.js
+++ src/index.js
@@ -1,17 +1,50 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React, { useState } from 'react'
+import ReactDOM from 'react-dom'
+import { ThemeProvider } from 'styled-components'
+import PrimaryButton, { 
+  SecondaryButton, 
+  TertiaryButton 
+} from './components/Buttons'
+import { GlobalStyle, darkTheme, defaultTheme } from './utils'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const App = () => {
+  const [useDarkTheme, setUseDarkTheme] = useState(false);
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+  return (
+    <ThemeProvider theme={useDarkTheme ? darkTheme : defaultTheme}>
+      <button 
+        style={{ margin: "0 16px 24px", padding: "8px", background: "none" }}
+        onClick={() => setUseDarkTheme(true)}
+      >
+        Dark theme
+      </button>
+      <button 
+        style={{ margin: "0 16px 24px", padding: "8px", background: "none" }}
+        onClick={() => setUseDarkTheme(false)}
+      >
+        Default theme
+      </button>
+      <div style={{
+        background: useDarkTheme ? defaultTheme.primaryColor : darkTheme.primaryColor,
+        width: "100vw",
+        height: "100vh",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "space-around"
+      }}>
+        <PrimaryButton>
+          Hello world
+        </PrimaryButton>
+        <SecondaryButton>
+          Goodbye world
+        </SecondaryButton>
+        <TertiaryButton>
+          Hey world
+        </TertiaryButton>
+      </div>
+      <GlobalStyle />
+    </ThemeProvider>
+  )
+}
+
+ReactDOM.render(<App />, document.querySelector('#root'))
